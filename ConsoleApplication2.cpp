09776b8f97fcb919fd03a9117859e03cedfeac6e
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-03 20:54:19 +0300

    mass

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int mass[100];
+	int N;
+	cin >> N;
+	for  (int i = 0;i < N; i++)
+	{
+		cin >>mass[i];
+	}
+	int B;
+	cin >> B;
+		int summ = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if ( mass[i]<=B)
+			{
+				summ += mass[i];
+			}
+		}
+		cout << summ;
+}
